53f2bdf406f122b368631115cefbfd923ba22aa1
MohneeshParkhi <mohneesh0088@gmail.com>
2018-07-03 18:04:44 +0530

    first commit after designing app, contains flaws

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shoppingList/shopping-edit/shopping-edit.component';
+import { RecipeComponent } from './recipe/recipe/recipe.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
